41f72ac74f2b23b49665def430b66b328365d09a
Aristide Sessa <xaristidex@icloud.com>
2019-02-12 14:35:29 +0100

    Initial Commit

---
+++ NewiFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewiFun
+//
+//  Created by Aristide Sessa on 12/02/19.
+//  Copyright © 2019 Safety. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
