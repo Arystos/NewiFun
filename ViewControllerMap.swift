9231f9a6bd04407d7da41af2be37dc4070ffcf31
Aristide Sessa <xaristidex@icloud.com>
2019-02-12 15:38:34 +0100

    15:38 Aggiunta dello span e della region nella mappa, per zoomare sulla posizione attuale

--- ViewControllerMap.swift
+++ ViewControllerMap.swift
@@ -7,27 +7,44 @@
 //
 
 import UIKit
-import MapKit
-import CoreLocation
+import MapKit // Serve per importare i comandi della mappa
+import CoreLocation // Imposta i comandi della Localizzazione
 
 class ViewControllerMap: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
-    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapView: MKMapView! // Reference alla mapView
     
-    var managerPosizione: CLLocationManager!
-    var posizioneUtente: CLLocationCoordinate2D!
+    var locationManager: CLLocationManager! // variabile che gestisce la posizione
+    var userPosition: CLLocationCoordinate2D! // variabile che imposta la posizione utente
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.mapView.delegate = self
-        self.managerPosizione = CLLocationManager()
+        self.mapView.delegate = self // Delegato alla gestione del ViewControllerMap
+        self.locationManager = CLLocationManager() // inizializzazione della CLLocation in locationManager
         
-        managerPosizione.delegate = self
-        managerPosizione.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        managerPosizione.requestWhenInUseAuthorization()
-        managerPosizione.startUpdatingLocation()
+        locationManager.delegate = self // locationManager delega CLLocationManager
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters //Precisione della posizione
+        locationManager.requestWhenInUseAuthorization() //Se non è attiva la posizione è richiesta la geolocalizzazione
+        locationManager.startUpdatingLocation() //Recupera e aggiorna gli spostamenti utente
         
+    }
+    
+    // Funzione che imposta la visualizzazione a seguire il punto
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        self.userPosition = userLocation.coordinate
+        
+        print("update position - lat: \(userLocation.coordinate.latitude) long: \(userLocation.coordinate.longitude)") // Print a console delle coordinate aggiornate
+        
+        //let span = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03) // span che imposta lo zoom
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        let region = MKCoordinateRegion(center: userPosition, span: span) // Regione visualizzata ogniqualvolta si aggiorna
+        
+        mapView.setRegion(region, animated: true) // Animazione della region quando zoomma
+        
+        self.mapView.showsUserLocation = true
     }
     
 
